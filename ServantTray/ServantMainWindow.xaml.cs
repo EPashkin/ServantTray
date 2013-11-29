dee7d3dd1c4b0649a15e2cabc70dcdd62923aaed
Evgeny Pashkin <eapashkin@gmail.com>
2013-11-29 14:47:30 +0400

    Exit command moved to View-Model
    (all working, but XAML editor shows error about ExitCanExecute)

--- ServantTray/ServantMainWindow.xaml.cs
+++ ServantTray/ServantMainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ServantTray
 {
@@ -24,31 +11,16 @@ namespace ServantTray
 
         public MainWindow()
         {
-            InitializeComponent();
+            var vm = new ServantMainVM();
+            this.DataContext = vm;
 
-            this.TaskIcon.TrayContextMenuOpen += TaskIcon_TrayContextMenuOpen;
+            InitializeComponent();
 
             //hide Main Window
             this.Visibility = System.Windows.Visibility.Hidden;
 
             string target = "ketchup";
             OTP = new OTP_worker(target);
-        }
-
-        void TaskIcon_TrayContextMenuOpen(object sender, RoutedEventArgs e)
-        {
-            //hack: for enabling 'exit' menu item
-            this.TaskIcon.ContextMenu.Focus();
-        }
-
-        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-            e.Handled = true;
-        }
-        private void OnExit(object sender, ExecutedRoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
         }
     }
 }
